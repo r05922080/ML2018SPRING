ce7db6bb5bdcf6ccebbda649dc2ed05c3047c009
daniel820710 <r05922080@ntu.edu.tw>
2018-06-07 11:43:11 +0800

    v3

--- hw5/hw5_test.sh
+++ hw5/hw5_test.sh
@@ -1,3 +1,3 @@
 #/usr/bin bash
-wget 'https://www.dropbox.com/s/ezzr44ph4kfr8ot/model.h5?dl=1' -O "model.h5"
+wget "https://www.dropbox.com/s/saklw40rvxwnlwm/model.h5?dl=1" -O "model.h5" 
 python3 hw5_test.py $1 $2 $3
